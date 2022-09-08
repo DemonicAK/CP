5a9d5bce70bfcce45e5b37d1670d10bbab6589a5
Arijit kar <arijitkar29@gmail.com>
2022-09-08 16:28:41 +0530

    f

---
+++ try.cpp
@@ -0,0 +1,41 @@
+#include "bits/stdc++.h"
+using namespace std;
+	
+
+
+
+
+
+void solve(){
+	
+
+
+
+
+
+ cout<<"i am arijit kar";
+
+
+
+
+
+
+
+
+	
+}
+
+
+
+
+
+	
+int main(){
+	int t;
+	cin>>t;
+	while(t){
+	solve();
+	t--;
+	}
+	return 0;
+}
